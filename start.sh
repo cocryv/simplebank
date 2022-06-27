01d029ecb4b25d80eb9ab32409051345239f950e
cocryv <60353356+cocryv@users.noreply.github.com>
2022-06-27 23:25:29 +0200

    fix sh env variable sourcee

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@
 set -e
 
 echo "run db migration"
+# ici on utilise source car on veut montrer ou son les variable d'environnement dans notre réseau de conteneur
+
+source /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app" 
